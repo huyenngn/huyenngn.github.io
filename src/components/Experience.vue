6cc5461d4e9902b8b12cbc25eb910fc6096a6334
huyenngn <ngan-huyen.thi-nguyen@deutschebahn.com>
2024-11-01 17:01:56 +0100

    refactor: Update work experience

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -4,16 +4,10 @@ import Item from './ProjectsItem.vue';
 
 const jobs = [
     {
-        title: "🚄 Python Developer Working Student @ DB InfraGo",
-        text: "Worked part-time as a Python Developer, starting in 03/2024. Maintained and contributed to the System Engineering Toolchain at DB InfraGo. Tool development lead of several internal and open-source projects.",
+        title: "🚄 Software Developer @ DB InfraGo",
+        text: "<ul><li>Full-time internship, starting in 09/2023, and subsequent part-time work as a Software Development Working Student since 03/2024</li><li>Maintained and contributed to the System Engineering Toolchain at DB InfraGo that aid 'AutomatedTrain' and other DBS projects</li><li>Shipped the Cable Tree View for the py-capellambse 'Context Diagrams' extension among other features</li><li>Tool development lead of Capella ROS Tools</li><li>Frontend developement lead of Polarion Config Diff Tool</li><li>Contributed to py-capellambse e.g. 'sync' operation of the 'decl' module</li><li>Documentation, testing, and CI/CD with Github Actions</li>",
         links: ["https://github.com/DSD-DBS/py-capellambse", "https://github.com/DSD-DBS/capellambse-context-diagrams",],
-        technologies: ["Python", "Capella MBSE", "React", "FastAPI", "TailwindCSS"],
-    },
-    {
-        title: "🚄 Internship @ DB InfraGo",
-        text: "Full-time internship, starting in 09/2023. <ul><li>Document generation</li><li>CapellaMBSE model transformation</li><li>Context diagram generation</li><li>Algorithm development for context collection</li><li>Documentation, testing, and CI/CD</li>",
-        links: ["https://github.com/DSD-DBS/capella-ros-tools"],
-        technologies: ["Python"],
+        technologies: ["Python", "Vue.js", "React", "FastAPI", "TailwindCSS", "GitHub Actions",],
     },
 ];
 </script>

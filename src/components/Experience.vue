5666f8ac52c77844339581cea84352ba64848542
huyenngn <ngan-huyen.thi-nguyen@deutschebahn.com>
2024-11-01 15:30:15 +0100

    style: Change primary color

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -5,13 +5,13 @@ import Item from './ProjectsItem.vue';
 const jobs = [
     {
         title: "🚄 Python Developer Working Student @ DB InfraGo",
-        text: "Worked part-time as a Python Developer, starting in 03/2024. Maintained and contributed to the System Engineering Toolchain at DB InfraGo. Tool development lead of several internal and open-source projects.  <ul><li>Document generation</li><li>SVG rendering</li><li>Capella model transformation</li><li>Context diagram generation</li><li>Algorithm development for context collection</li><li>Documentation, testing, and CI/CD</li>",
+        text: "Worked part-time as a Python Developer, starting in 03/2024. Maintained and contributed to the System Engineering Toolchain at DB InfraGo. Tool development lead of several internal and open-source projects.",
         links: ["https://github.com/DSD-DBS/py-capellambse", "https://github.com/DSD-DBS/capellambse-context-diagrams",],
         technologies: ["Python", "Capella MBSE", "React", "FastAPI", "TailwindCSS"],
     },
     {
         title: "🚄 Internship @ DB InfraGo",
-        text: "Full-time internship, starting in 09/2023.",
+        text: "Full-time internship, starting in 09/2023. <ul><li>Document generation</li><li>CapellaMBSE model transformation</li><li>Context diagram generation</li><li>Algorithm development for context collection</li><li>Documentation, testing, and CI/CD</li>",
         links: ["https://github.com/DSD-DBS/capella-ros-tools"],
         technologies: ["Python"],
     },
